7fc2e5aa0be9c76c95cdae16c57aa829bf786855
Alejandro Velardo <a.velardo.lucero@gmail.com>
2023-02-06 16:59:43 +0100

    Implementing router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,7 @@
     <header>
         <h1> {{ title }}</h1>
         <Button 
+        v-show="homePage"
         @btn-click="$emit('toggle-add-task')" 
         :text="showAddTask ? 'Close' : 'Add Task'" 
         :color="showAddTask ? 'Red' : 'Green'" />
@@ -17,6 +18,15 @@ import Button from './Button'
         },
         components: {
             Button
+        },
+        computed: {
+            homePage() {
+                if(this.$route.path === '/' ) {
+                    return true
+                } else {
+                    return false
+                }
+            }
         }
     }
 </script>
